c49e82c054be16056e7369fec825a35b501671b4
Aylikkus <4l1kku@gmail.com>
2023-05-20 19:43:13 +0300

    Add tests

---
+++ Tests/DisciplineTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Atlas.Data;
+
+namespace RPDGenerator.Tests
+{
+    [TestClass]
+    public class DisciplineTests
+    {
+        [TestMethod]
+        public void Abbrevation_TLPRReturn()
+        {
+            string discName = "Технологии личностно-профессионального развития";
+            Discipline disc = new Discipline("example", discName);
+
+            string expected = "ТЛПР";
+
+            Assert.AreEqual(expected, disc.Abbrevation);
+        }
+
+        [TestMethod]
+        public void Abbrevation_EReturn()
+        {
+            string discName = "Экономика";
+            Discipline disc = new Discipline("example", discName);
+
+            string expected = "Э";
+
+            Assert.AreEqual(expected, disc.Abbrevation);
+        }
+
+        [TestMethod]
+        public void Abbrevation_ScopeReturn()
+        {
+            string discName = "История (история России, всеобщая история)";
+            Discipline disc = new Discipline("example", discName);
+
+            string expected = "И(ИРВИ)";
+
+            Assert.AreEqual(expected, disc.Abbrevation);
+        }
+    }
+}
---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e161d2e1-ee09-4e52-8642-23699c0a2a2e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
