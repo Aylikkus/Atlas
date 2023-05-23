6ff4244fb5ffa5c95737b24f8ad8592d3b354547
Aylikkus <4l1kku@gmail.com>
2023-05-23 15:28:47 +0300

    tmp testing while no UI

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Atlas.Interops.LibreOffice;
+﻿using Atlas.Data;
+using Atlas.Interops.LibreOffice;
+using Atlas.Interops.Office;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,9 +18,11 @@ namespace Atlas
         public MainWindow()
         {
             InitializeComponent();
-
-            CalcReader cr = new CalcReader();
-            cr.PullAttributes("");
+            string pwd = Environment.CurrentDirectory;
+            string prjFolder = pwd.Remove(pwd.IndexOf("Atlas") + 6);
+            DocAttributes da = new DocAttributes();
+            using (CalcReader cr = new CalcReader())
+                da = cr.PullAttributes(prjFolder + @"Reference\10.05.04___2021_56_.plx.xlsx");
         }
     }
 }
